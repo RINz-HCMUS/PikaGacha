ff4ae970a952bf3c31864e6603f06d13373c7734
RINz-HCMUS <tuanv2322004@gmail.com>
2023-04-15 14:49:31 +0700

    final

--- Pikachu/Sound.h
+++ Pikachu/Sound.h
@@ -1,25 +1,32 @@
 #pragma once
 #include "SettingGame.h"
-#include <MMSystem.h>
-#include <stdio.h>
+
 //#pragma comment(lib, "winmm.lib")
-#include <iostream>
 struct sound{
+	// âm thanh khi di chuyển
 	static void choose();
 	
+	// âm thanh khi chọn đúng
 	static void correct();
 	
+	// âm thanh khi vào LEADERBOARD
 	static void leaderboard();
 	
+	//âm thanh khi bắt đầu màn chơi
 	static void start();
 	
+	// âm thanh khi chọn sai
 	static void wrong();
 	
+	// âm thanh khi chiến thắng
 	static void victory();
 	
+	// âm thanh ở Menu chính
 	static void theme();
 	
+	// âm thanh khi vào POKEDEX
 	static void pokedexTheme();
 	
+	// tắt âm
 	static void off();
 };
--- Pikachu/main.cpp
+++ Pikachu/main.cpp
@@ -9,10 +9,10 @@ int main()
 	Menu MainMenu;
 	
 	//	Khoi tao
-	account newAccount;
-	account a[100];
-	int stt = 0;
-    int loginComplete = 0;
+	account newAccount; // account đang đăng nhập
+	account a[100];	// danh sách account đã tạo
+	int stt = 0;	// số lượng account đã tạo
+    int loginComplete = 0;	// trạng thái đăng nhập  0: đăng kí / 1: đăng nhập thành công / 2: đăng nhập lại
 
     //	Truyen du lieu tu file
 	std::ifstream ifs("rank.txt", std::ios::in);
@@ -33,178 +33,175 @@ int main()
     stt--;
     ifs.close();
     
-	// Bat con tro	
-while(loginComplete != 1){
-	loginComplete = 0;
-	MainMenu.printLOGIN();
-	SettingGame::setCursorInput(1);
+	//xử lý đăng nhập	
+	while(loginComplete != 1){
+		loginComplete = 0;
+		MainMenu.printLOGIN();
+		SettingGame::setCursorInput(1); // Bật hiển thị con trỏ
+		
+		string s = "";
+		
+		// The first time
+		SettingGame::setColor(BLACK, RED);
+		SettingGame::gotoXY(85, 24);
+		std::cout << "Input Name";
 	
-	string s = "";
-	///	Input account name
-	
-	// The first time
-	SettingGame::setColor(BLACK, RED);
-	SettingGame::gotoXY(85, 24);
-	std::cout << "Input Name";
-
-	SettingGame::setColor(BLACK, WHITE);
-	SettingGame::gotoXY(85, 26);
-	std::cin >> s;
-
-	SettingGame::setColor(BLACK, BLACK);
-	SettingGame::gotoXY(85, 24);
-	std::cout << "          ";
-	
-    while(s.size() > 15 || s == ""){
-        SettingGame::clearConsole();
-        MainMenu.printLOGIN();
-        SettingGame::setColor(BLACK, RED);
-        SettingGame::gotoXY(85, 24);
-		cout << "The account name must not be empty and must not exceed 15 English characters.";
+		SettingGame::setColor(BLACK, WHITE);
 		SettingGame::gotoXY(85, 26);
+		std::cin >> s;
+	
+		SettingGame::setColor(BLACK, BLACK);
+		SettingGame::gotoXY(85, 24);
+		std::cout << "          ";
+		
+		// xử lý username không hợp lệ
+	    while(s.size() > 15 || s == ""){
+	        SettingGame::clearConsole();
+	        MainMenu.printLOGIN();
+	        SettingGame::setColor(BLACK, RED);
+	        SettingGame::gotoXY(85, 24);
+			cout << "The account name must not be empty and must not exceed 15 English characters.";
+			SettingGame::gotoXY(85, 26);
+			SettingGame::setColor(BLACK, WHITE);
+		    cin.ignore();
+			cin >> s;
+	    }
+	    
+		SettingGame::setColor(BLACK, BLACK);
+	    SettingGame::gotoXY(85, 24);
+		cout << "                                                                              ";
+		
+		strcpy(newAccount.Name, s.c_str());
+		
+		
+		SettingGame::setColor(BLACK, RED); 
+	    SettingGame::gotoXY(85, 24);
+		std::cout << "Input Pass";
+		SettingGame::gotoXY(85, 30);
 		SettingGame::setColor(BLACK, WHITE);
 	    cin.ignore();
 		cin >> s;
-    }
-    
-	SettingGame::setColor(BLACK, BLACK);
-    SettingGame::gotoXY(85, 24);
-	cout << "                                                                              ";
+		SettingGame::setColor(BLACK, BLACK);
+		SettingGame::gotoXY(85, 24);
+		std::cout << "          ";
+		
+		// xử lý password không hợp lệ
+		while(s.size() > 15 || s == ""){
+	        SettingGame::clearConsole();
+	        MainMenu.printLOGIN();
+	        SettingGame::setColor(BLACK, RED);
+	        SettingGame::gotoXY(85, 24);
+	        cout << "The account password must not be empty and must not exceed 15 character.";
+	        SettingGame::setColor(BLACK, WHITE);
+	        SettingGame::gotoXY(85, 30);
+	        cin.ignore();
+			cin >> s;
+	    }
+	    
+	    SettingGame::setColor(BLACK, BLACK);
+	    SettingGame::gotoXY(85, 24);
+		cout << "                                                                         ";
+	    strcpy(newAccount.Pass, s.c_str());    
+	    
+	    // kiểm tra xem account đã được tạo chưa
+		for(int i = 0; i < stt; i++){
+			if(loginComplete == 1)
+				break;
+			// Neu tai khoan ton tai
+	        if(strcmp(a[i].Name, newAccount.Name) == 0){
+	        	// check password
+	            if(strcmp(a[i].Pass, newAccount.Pass) == 0){
+				SettingGame::setColor(BLACK, RED);
+	            SettingGame::gotoXY(85, 24);
+					cout << "LOGIN COMPLETE!";
+					loginComplete = 1;
+	            }
 	
-	strcpy(newAccount.Name, s.c_str());
-	
-	
-	SettingGame::setColor(BLACK, RED); 
-    SettingGame::gotoXY(85, 24);
-	std::cout << "Input Pass";
-	SettingGame::gotoXY(85, 30);
-	SettingGame::setColor(BLACK, WHITE);
-    cin.ignore();
-	cin >> s;
-	SettingGame::setColor(BLACK, BLACK);
-	SettingGame::gotoXY(85, 24);
-	std::cout << "          ";
-	
-	while(s.size() > 15 || s == ""){
-        SettingGame::clearConsole();
-        MainMenu.printLOGIN();
-        SettingGame::setColor(BLACK, RED);
-        SettingGame::gotoXY(85, 24);
-        cout << "The account password must not be empty and must not exceed 15 character.";
-        SettingGame::setColor(BLACK, WHITE);
-        SettingGame::gotoXY(85, 30);
-        cin.ignore();
+	            else{
+	                int tmp = 3;
+	                while(tmp){
+						SettingGame::setColor(BLACK, RED);
+	           			SettingGame::gotoXY(85, 24);
+						cout << "Wrong password. Try agian." << tmp << " times left.\n";
+	                    SettingGame::gotoXY(85, 30);
+						cin.ignore();
+	                    cin >> s;
+	                    if(strcmp(a[i].Pass, s.c_str()) == 0){
+	                    	strcpy(newAccount.Pass, a[i].Pass);
+							break; 
+						}
+	                     else
+	                        tmp--;
+	                }
+	                if(tmp == 0){
+	                    SettingGame::setColor(BLACK, BLACK);
+	    	    		SettingGame::gotoXY(85, 24);
+						cout << "   				                             		";
+						SettingGame::setColor(BLACK, RED);
+	           			SettingGame::gotoXY(85, 24);
+						cout << "You have entered the wrong password too many times!";
+	                    loginComplete = 2;
+	                }
+	                else{
+					
+		                SettingGame::setColor(BLACK, BLACK);
+	    		    	SettingGame::gotoXY(85, 24);
+						cout << "   		                         				";
+						SettingGame::setColor(BLACK, RED);
+	           			SettingGame::gotoXY(85, 24);
+	            	    cout << "LOGIN COMPLETE!";
+	                	loginComplete = 1;
+	                }
+	            }
+	        }
+	    }
+	    
+	    // Đăng ký
+	    if(loginComplete == 0){
+	    SettingGame::setColor(BLACK, RED);
+		SettingGame::gotoXY(75, 24);	
+	    cout << "Don't have this accound. Do you want sign in it? (1: YES / 0: NO)\n";
+	    SettingGame::setColor(BLACK, WHITE);
+		SettingGame::gotoXY(75, 25);
 		cin >> s;
-        }
-    SettingGame::setColor(BLACK, BLACK);
-    SettingGame::gotoXY(85, 24);
-	cout << "                                                                         ";
-    strcpy(newAccount.Pass, s.c_str());    
-    
-	for(int i = 0; i < stt; i++){
-		if(loginComplete == 1)
-			break;
-		// Neu tai khoan ton tai
-        if(strcmp(a[i].Name, newAccount.Name) == 0){
-        	// check password
-            if(strcmp(a[i].Pass, newAccount.Pass) == 0){
-			SettingGame::setColor(BLACK, RED);
-            SettingGame::gotoXY(85, 24);
-				cout << "LOGIN COMPLETE!";
+		
+		while(true)
+	        if(s == "YES" || s == "1"){
+	        	SettingGame::gotoXY(75, 25);
+	            cout << "Your account was created! Have fun.";
 				loginComplete = 1;
-            }
-
-            else{
-                int tmp = 3;
-                while(tmp){
-					SettingGame::setColor(BLACK, RED);
-           			SettingGame::gotoXY(85, 24);
-					cout << "Wrong password. Try agian." << tmp << " times left.\n";
-                    SettingGame::gotoXY(85, 30);
-					cin.ignore();
-                    cin >> s;
-                    if(strcmp(a[i].Pass, s.c_str()) == 0){
-                    	strcpy(newAccount.Pass, a[i].Pass);
-						break; 
-					}
-                     else
-                        tmp--;
-                }
-                if(tmp == 0){
-                    SettingGame::setColor(BLACK, BLACK);
-    	    		SettingGame::gotoXY(85, 24);
-					cout << "   				                             		";
-					SettingGame::setColor(BLACK, RED);
-           			SettingGame::gotoXY(85, 24);
-					cout << "You have entered the wrong password too many times!";
-                    loginComplete = 2;
-                }
-                else{
+				newAccount.Mode = 0;
+				newAccount.Score = 0;
+	        	Sleep(500);
+	            break;
+	        }
+	        
+	        else if(s == "NO" || s == "0"){
+	        	SettingGame::gotoXY(75, 25);
+	            cout << "Please login again!";
+	            Sleep(500);
+	            break;
+	        }
+	        else{
+				MainMenu.printLOGIN();
+				SettingGame::setColor(BLACK, WHITE);
+	        	SettingGame::gotoXY(85, 26);
+	        	cout << newAccount.Name;
 				
-	                SettingGame::setColor(BLACK, BLACK);
-    		    	SettingGame::gotoXY(85, 24);
-					cout << "   		                         				";
-					SettingGame::setColor(BLACK, RED);
-           			SettingGame::gotoXY(85, 24);
-            	    cout << "LOGIN COMPLETE!";
-                	loginComplete = 1;
-                }
-            }
-        }
-    }
-    if(loginComplete == 0){
-    SettingGame::setColor(BLACK, RED);
-	SettingGame::gotoXY(75, 24);	
-    cout << "Don't have this accound. Do you want sign in it? (1: YES / 0: NO)\n";
-    SettingGame::setColor(BLACK, WHITE);
-	SettingGame::gotoXY(75, 25);
-	cin >> s;
+				SettingGame::gotoXY(85, 30);
+	        	cout << newAccount.Pass;
 	
-	while(true)
-        if(s == "YES" || s == "1"){
-        	SettingGame::gotoXY(75, 25);
-            cout << "Your account was created! Have fun.";
-			loginComplete = 1;
-			newAccount.Mode = 0;
-			newAccount.Score = 0;
-//			std::ofstream ofs("rank.txt", std::ios::app);
-//        	ofs << newAccount.Name << std::endl;
-//        	ofs << newAccount.Pass << std::endl;
-//        	ofs << newAccount.Mode << std::endl;
-//        	ofs << newAccount.Score << std::endl;
-//       	ofs.close();
-        	Sleep(500);
-            break;
-        }
-        
-        else if(s == "NO" || s == "0"){
-        	SettingGame::gotoXY(75, 25);
-            cout << "Please login again!";
-            Sleep(500);
-            break;
-        }
-        else{
-			MainMenu.printLOGIN();
-			SettingGame::setColor(BLACK, WHITE);
-        	SettingGame::gotoXY(85, 26);
-        	cout << newAccount.Name;
-			
-			SettingGame::gotoXY(85, 30);
-        	cout << newAccount.Pass;
-
-   			SettingGame::setColor(BLACK, RED);
-			SettingGame::gotoXY(75, 24);	
-    		cout << "Don't have this accound. Do you want sign in it? (1: YES / 0: NO)\n";       	
-    		
-        	SettingGame::gotoXY(75, 25);
-            cin >> s;
-        }
-  	}
-}
-	SettingGame::setCursorInput(0);
-//	MainMenu.printVictory();
+	   			SettingGame::setColor(BLACK, RED);
+				SettingGame::gotoXY(75, 24);	
+	    		cout << "Don't have this accound. Do you want sign in it? (1: YES / 0: NO)\n";       	
+	    		
+	        	SettingGame::gotoXY(75, 25);
+	            cin >> s;
+	        }
+	  	}
+	}
+	SettingGame::setCursorInput(0); // Tắt  hiển thị con trỏ
+	// Bắt đầu game
 	while(true)
 		MainMenu.printMenu(newAccount);
-
-
 }
